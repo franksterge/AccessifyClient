03643bb1479e8a7dd96c433c37a495d6d0e175c1
Jonathan Cady <joncady@uw.edu>
2019-01-12 18:42:06 -0800

    Updates

---
+++ precache-manifest.2e3ebea9400c7245528b5d7aac58fcea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b1299ce80295cef8090be98a93b5da6",
+    "url": "/AccessifyClient/static/media/profile.4b1299ce.png"
+  },
+  {
+    "revision": "5390b86274ef05dbc8b586b446354914",
+    "url": "/AccessifyClient/static/media/pic2.5390b862.png"
+  },
+  {
+    "revision": "d3ebc207169bf3969d7a",
+    "url": "/AccessifyClient/static/js/runtime~main.d3ebc207.js"
+  },
+  {
+    "revision": "460947184ca26ab141d9",
+    "url": "/AccessifyClient/static/js/main.46094718.chunk.js"
+  },
+  {
+    "revision": "874068680c7a0ba9c462",
+    "url": "/AccessifyClient/static/js/1.87406868.chunk.js"
+  },
+  {
+    "revision": "460947184ca26ab141d9",
+    "url": "/AccessifyClient/static/css/main.22d79ce8.chunk.css"
+  },
+  {
+    "revision": "874068680c7a0ba9c462",
+    "url": "/AccessifyClient/static/css/1.740f0ad8.chunk.css"
+  },
+  {
+    "revision": "dab6651654ca7b522052c14b5d3d11ce",
+    "url": "/AccessifyClient/index.html"
+  }
+];
