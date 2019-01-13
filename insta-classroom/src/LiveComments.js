e5ebd772395dbb92165cea7d4c47175f26ee5597
Connie Wang <2conniewang@gmail.com>
2019-01-12 18:54:54 -0800

    realtime updates

--- insta-classroom/src/LiveComments.js
+++ insta-classroom/src/LiveComments.js
@@ -5,7 +5,8 @@ export default class LiveComments extends Component {
     constructor() {
         super();
         this.state = {
-            comments: {}
+            comments: {},
+            timer: null
         }
     }
 
@@ -14,28 +15,44 @@ export default class LiveComments extends Component {
         let name = this.props.name.replace(".mp4", "");
 
         var ref = firebase.database().ref(name);
-        ref.once("value")
-          .then((snapshot) => {
-            console.log(snapshot.val());
+        // ref.once("value")
+        //   .then((snapshot) => {
+        //     console.log(snapshot.val());
 
-            setInterval(() => {
-                let time = Math.round(video.currentTime);
-                // console.log(time);
-                // console.log(snapshot.val()[time]);
-                    if(snapshot.val() !== null && snapshot.val()[time] != null) {
-                        console.log(snapshot.val()[time]);
-                        if(this.state.comments[time] == null) {
-                            let curr = this.state.comments;
-                            curr[time] = snapshot.val()[time];
-                            this.setState(curr);
-                            console.log(this.state.comments);
-                        }
-                        for(let i = 0; i < time; i++) {
+        //     setInterval(() => {
+        //         let time = Math.round(video.currentTime);
+        //         // console.log(time);
+        //         // console.log(snapshot.val()[time]);
+        //             if(snapshot.val() !== null && snapshot.val()[time] != null) {
+        //                 console.log(snapshot.val()[time]);
+        //                 if(this.state.comments[time] == null) {
+        //                     let curr = this.state.comments;
+        //                     curr[time] = snapshot.val()[time];
+        //                     this.setState(curr);
+        //                     console.log(this.state.comments);
+        //                 }
+        //                 for(let i = 0; i < time; i++) {
                             
-                        }
-                    }
-            }, 1000);
-          });
+        //                 }
+        //             }
+        //     }, 1000);
+        //   });
+
+          setInterval(() => {
+            ref.once("value")
+            .then((snapshot) => {
+              console.log(snapshot.val());
+              if(snapshot.val()) {
+                  let curr = {};
+                  for(let i = 0; i < video.currentTime; i++) {
+                      if(snapshot.val()[i] != null) {
+                        curr[i] = snapshot.val()[i];
+                      }
+                  }
+                  this.setState({comments: curr});
+              }
+            })
+          }, 1000);
 
     }
 
